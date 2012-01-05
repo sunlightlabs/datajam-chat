573d9402c3258b9c92caeb430d238da90d7e1543
Dan Drinkard <dan.drinkard@gmail.com>
2012-01-05 17:33:27 -0500

    different angle w/ asset pipeline

--- lib/datajam_chat/engine.rb
+++ lib/datajam_chat/engine.rb
@@ -1,4 +1,5 @@
 require 'rails/engine'
+require 'sprockets/railtie'
 require 'datajam_chat'
 require 'bitly'
 
@@ -21,11 +22,17 @@ module DatajamChat
       end
     end
 
+    initializer "datajam.precompile_everything" do
+      Datajam.setup do |app|
+        app.assets.precompile += %w( *.js *.css )
+      end
+    end
+
     initializer "bitly.use_api_version_3" do
       Bitly.use_api_version_3
     end
 
-    if Rails.env =~ /^(development|test|production)$/
+    if Rails.env =~ /^(development|test)$/
       initializer "datajam_chat.static_assets" do |app|
         app.middleware.use ::ActionDispatch::Static, "#{root}/app/assets"
       end
