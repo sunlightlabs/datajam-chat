a2aed910a9ce46a6a8a004224d76a2b0d3cb6054
Dan Drinkard <dan.drinkard@gmail.com>
2012-01-26 13:25:39 -0500

    rename callbacks for compatibility with new system

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,12 +1,12 @@
 class Setting
   # callbacks to be run after save
-  def datajam_chat_bitly_login_callback
+  def datajam_chat_bitly_username_callback
     query_opts = DatajamChat.bitly.instance_variable_get('@default_query_opts')
     query_opts.update(:login => value)
     DatajamChat.bitly.instance_variable_set('@default_query_opts', query_opts)
   end
 
-  def datajam_chat_bitly_apikey_callback
+  def datajam_chat_bitly_api_key_callback
     query_opts = DatajamChat.bitly.instance_variable_get('@default_query_opts')
     query_opts.update(:apiKey => value)
     DatajamChat.bitly.instance_variable_set('@default_query_opts', query_opts)
