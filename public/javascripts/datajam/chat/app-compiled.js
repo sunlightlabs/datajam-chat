e6050305d9c2ec172fce30acdf21c3f89c6004bd
Dan Drinkard <dan.drinkard@gmail.com>
2012-07-03 17:15:47 -0400

    fix file uploading

--- public/javascripts/datajam/chat/app-compiled.js
+++ public/javascripts/datajam/chat/app-compiled.js
@@ -782,7 +782,7 @@
                       // iframe transport w/ jquery.form doesn't seem to be aware of status codes.
                       // so, we check for a url attribute to know if it worked.
                       if(response && response.url){
-                        var textarea = $(evt.target).parents('.inputArea').find('textarea');
+                        var textarea = $(evt.target).prev('form').find('textarea');
                         textarea.val(textarea.val() + location.protocol + '//' + location.host + response.url);
                         $(evt.target).val('');
                         textarea.focus();
